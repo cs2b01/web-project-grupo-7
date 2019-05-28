194bb8b2cd4027957d8732339a3ed9d39df5645e
AejandroGH97 <alejandrogoicochea@gmail.com>
2019-05-28 17:39:24 -0500

    First commit

--- web/server.py
+++ web/server.py
@@ -5,16 +5,31 @@ import json
 
 db = connector.Manager()
 engine = db.createEngine()
+db_session = db.getSession(engine)
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route("/index/")
+def main():
     return render_template('index.html')
 
-@app.route('/static/<content>')
-def static_content(content):
-    return render_template(content)
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/restaurantes/")
+def restaurantes():
+    restaurantes = db_session.query(entities.Restaurant.name).all()
+    return render_template('restaurantes.html', restaurantes=restaurantes)
+
+@app.route("/restaurantes/<id>/menu")
+def menu(id):
+    return render_template('menu.html')
+
+@app.route("/restaurantes/<id>/personal")
+def personal(id):
+    return render_template('personal.html')
 
 
 if __name__ == '__main__':
