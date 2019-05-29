7a5ac3a99d54743af3f60d94c499d6a5d4e4301f
AejandroGH97 <alejandrogoicochea@gmail.com>
2019-05-29 07:51:46 -0500

    Pagina para nuevo restaurante

--- web/model/entities.py
+++ web/model/entities.py
@@ -5,25 +5,25 @@ from database import connector
 class Restaurant(connector.Manager.Base):
     __tablename__ = 'restaurants'
     id = Column(Integer, Sequence('book_id_seq'), primary_key=True)
-    name = Column(String(50))
-    owner = Column(String(12))
-    address = Column(String(120))
-    phone_number = Column(String(120))
+    name = Column(String(50), nullable=False)
+    owner = Column(String(12), nullable=False)
+    address = Column(String(120), nullable=False)
+    phone_number = Column(String(120), nullable=False)
 
 class Plate(connector.Manager.Base):
     __tablename__ = 'plate'
     id = Column(Integer, Sequence('book_id_seq'), primary_key=True)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    name = Column(String(50))
+    name = Column(String(50), nullable=False)
     ingredients = Column(String(12))
     price = Column(String(120))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
     restaurant = relationship(Restaurant)
 
 class Employers(connector.Manager.Base):
     __tablename__ = 'employers'
     id = Column(Integer, Sequence('book_id_seq'), primary_key=True)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(12), nullable=False)
+    position = Column(String(12), nullable=False)
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    name = Column(String(50))
-    lastname = Column(String(12))
-    position = Column(String(12))
     restaurant = relationship(Restaurant)
--- web/server.py
+++ web/server.py
@@ -1,11 +1,13 @@
-from flask import Flask,render_template, request, session, Response, redirect
+from flask import Flask,render_template, request, session, Response, redirect, url_for
 from database import connector
 from model import entities
 import json
 
 db = connector.Manager()
 engine = db.createEngine()
-db_session = db.getSession(engine)
+
+
+
 
 
 app = Flask(__name__)
@@ -20,16 +22,42 @@ def home():
 
 @app.route("/restaurantes/")
 def restaurantes():
+    db_session = db.getSession(engine)
     restaurantes = db_session.query(entities.Restaurant.name).all()
     return render_template('restaurantes.html', restaurantes=restaurantes)
+
+@app.route("/restaurantes/new", methods=['POST','GET'])
+def nuevo_restaurante():
+    if request.method == "GET":
+        return render_template('nuevo_restaurante.html')
+
+    else:
+        db_session = db.getSession(engine)
+        nombre = request.form.get('nombre')
+        _owner = request.form.get('owner')
+        direccion = request.form.get('direccion')
+        telefono = request.form.get('telefono')
+
+        restaurante = entities.Restaurant(name=nombre, owner=_owner, address=direccion, phone_number=telefono)
+        db_session.add(restaurante)
+        db_session.commit()
+        return redirect(url_for('restaurantes'))
 
 @app.route("/restaurantes/<id>/menu")
 def menu(id):
     return render_template('menu.html')
 
+@app.route("/restaurantes/<id>/menu/new")
+def nuevo_menu(id):
+    return render_template('nuevo_plato.html')
+
 @app.route("/restaurantes/<id>/personal")
 def personal(id):
     return render_template('personal.html')
+
+@app.route("/restaurantes/<id>/personal/new")
+def nuevo_personal(id):
+    return render_template('nuevo_personal.html')
 
 
 if __name__ == '__main__':
